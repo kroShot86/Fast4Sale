7a495981c02ff339d98a83543e24e64a4ff40ca7
”kroShot” <kroShot86@mail.ru>
2026-01-04 13:25:36 +1000

    Добавьте файлы проекта.

---
+++ Fast4Sale/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fast4Sale
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Fast4Sale/New_ob.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fast4Sale
+{
+    /// <summary>
+    /// Логика взаимодействия для New_ob.xaml
+    /// </summary>
+    public partial class New_ob : Window
+    {
+        public New_ob()
+        {
+            InitializeComponent();
+        }
+    }
+}
